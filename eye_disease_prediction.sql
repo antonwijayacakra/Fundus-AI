54898156fbdb5124a5a65662c3886db0b1e4f507
antonwijayacakra <asangwijaya2017@gmail.com>
2024-06-23 23:09:16 +0700

    Selesai dan memperbarui file SQL

--- eye_disease_prediction.sql
+++ eye_disease_prediction.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2024 at 06:28 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 23 Jun 2024 pada 18.08
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,72 +24,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `prediction`
+-- Struktur dari tabel `prediction`
 --
 
 CREATE TABLE `prediction` (
   `id` int(11) NOT NULL,
   `filename` varchar(255) NOT NULL,
   `prediction` varchar(50) NOT NULL,
-  `upload_date` datetime DEFAULT NULL
+  `upload_date` datetime DEFAULT NULL,
+  `model` char(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `prediction`
+-- Dumping data untuk tabel `prediction`
 --
 
-INSERT INTO `prediction` (`id`, `filename`, `prediction`, `upload_date`) VALUES
-(1, 'cataract_061.png', 'cataract', '2024-06-10 22:50:54'),
-(3, '3386_left.jpg', 'normal', '2024-06-10 23:09:03'),
-(5, 'images.jpeg', 'glaucoma', '2024-06-10 23:24:12'),
-(6, 'depositphotos_568325816-stock-photo-proliferative-diabetic-retinopathy-illustration-showing.jpg', 'retinopathy', '2024-06-10 23:24:27'),
-(8, 'Screenshot_2024-06-10_204847.jpg', 'cataract', '2024-06-11 16:54:35'),
-(9, 'cataract_061.png', 'cataract', '2024-06-11 16:54:48');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `predictions`
---
-
-CREATE TABLE `predictions` (
-  `id` int(11) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `prediction` varchar(50) NOT NULL,
-  `upload_date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `prediction` (`id`, `filename`, `prediction`, `upload_date`, `model`) VALUES
+(63, 'DR.jpeg', 'DR', '2024-06-23 22:28:11', ''),
+(64, 'DR.jpeg', 'retinopathy', '2024-06-23 22:47:00', 'xception'),
+(65, 'DR.jpeg', 'retinopathy', '2024-06-23 22:48:21', 'xception'),
+(66, 'DR.jpeg', 'glaucoma', '2024-06-23 22:48:43', 'mobilenet'),
+(67, 'DR.jpeg', 'glaucoma', '2024-06-23 22:48:51', 'vgg16'),
+(68, 'DR.jpeg', 'glaucoma', '2024-06-23 22:50:08', 'mobilenet'),
+(69, 'DR.jpeg', 'glaucoma', '2024-06-23 22:50:08', 'mobilenet'),
+(70, 'DR.jpeg', 'glaucoma', '2024-06-23 22:50:15', 'vgg16'),
+(71, 'Glaucoma_test.jpg', 'normal', '2024-06-23 22:50:31', 'mobilenet'),
+(72, 'Glaucoma_test.jpg', 'normal', '2024-06-23 22:50:42', 'vgg16'),
+(73, 'DR.jpg', 'DR', '2024-06-23 22:52:05', 'xception'),
+(74, 'DR.jpg', 'DR', '2024-06-23 22:52:16', 'mobilenet'),
+(75, 'DR.jpg', 'DR', '2024-06-23 22:52:25', 'vgg16');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `prediction`
+-- Indeks untuk tabel `prediction`
 --
 ALTER TABLE `prediction`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `predictions`
---
-ALTER TABLE `predictions`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `prediction`
+-- AUTO_INCREMENT untuk tabel `prediction`
 --
 ALTER TABLE `prediction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `predictions`
---
-ALTER TABLE `predictions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
